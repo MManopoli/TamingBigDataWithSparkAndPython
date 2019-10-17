60503193011e2bd26cba5a47f478124b3477c59c
mmanopoli <matthew.manopoli@gmail.com>
2019-10-17 14:21:39 +0000

    Updating code with notes from lecture 13.  Fixed the print statement, also - bad python :-P

--- min-temperatures.py
+++ min-temperatures.py
@@ -1,4 +1,4 @@
-# Objective is to find the minimum temperature observed by each weather station
+# Objective is to find the minimum temperature observed by various weather stations in the year 1800
 from pyspark import SparkConf, SparkContext
 
 # Set Master as "local" - local machine + single thread / process
@@ -7,19 +7,33 @@ conf = SparkConf().setMaster("local").setAppName("MinTemperatures")
 # Instantiate SparkContext object with conf above
 sc = SparkContext(conf=conf)
 
+# Sample data line: "ITE00100554,18000101,TMAX,-75,,,E,"
+# Columns are "weather station id, date, entry type, temperature in tenths of a degree celsius, etc, etc, etc..."
 def parseLine(line):
     fields = line.split(',')
     stationID = fields[0]
     entryType = fields[2]
-    temperature = float(fields[3]) * 0.1 * (9.0 / 5.0) + 32.0
+    temperature = float(fields[3]) * 0.1 * (9.0 / 5.0) + 32.0  # Convert temp to degrees Fahrenheit
     return (stationID, entryType, temperature)
 
+# Import and parse the input data into an RDD of lines called lines
 lines = sc.textFile("/home/mmanopoli/Udemy/TamingBigDataWithSparkAndPython/data/1800.csv")
+# Use the parseLine fucntion defined above to parse the data into a usable RDD
 parsedLines = lines.map(parseLine)
+# The filter transformation takes a function that returns a boolean
+# In this case, we want the transform to return an RDD where only entries with "TMIN" as the [1] item are kept
+# This is because we are trying to find the minimum observed temperature by each weather station in 1800, and so
+#  we only need to consider the minimum temperature observed on any given  (labeled "TMIN")
 minTemps = parsedLines.filter(lambda x: "TMIN" in x[1])
+# Strip out the entryType because it's always "TMIN" now
+# This is now a key value pair, stationID = key, temperature = value
 stationTemps = minTemps.map(lambda x: (x[0], x[2]))
-minTemps = stationTemps.reduceByKey(lambda x, y: min(x,y))
-results = minTemps.collect();
+# reduce by key = for each key reduce down using the lambda function
+# So, the lambda function returns the minimum of two inputs, x and y, which defines the result when two elements
+#  x and y are reduced/combined for a given key
+minTemps = stationTemps.reduceByKey(lambda x, y: min(x, y))
+# Collect action - collect the results into a Python data structure
+results = minTemps.collect()
 
 for result in results:
-    print(result[0] + "\t{:.2f}F".format(result[1]))
+    print("{0}\t{1:.2f}F".format(result[0], result[1]))
