93e3d56e0126b1d0c2901d4c181b521607ccbc80
mmanopoli <matthew.manopoli@gmail.com>
2019-10-17 15:29:35 +0000

    Adding lecture 16 notes and cleaning the Python code.

--- word-count.py
+++ word-count.py
@@ -1,4 +1,4 @@
-# Objective is to count the number of words in a book
+# Objective is to count the number of words in a text file (Book.txt)
 from pyspark import SparkConf, SparkContext
 
 # Set Master as "local" - local machine + single thread / process
@@ -7,11 +7,18 @@ conf = SparkConf().setMaster("local").setAppName("WordCount")
 # Instantiate SparkContext object with conf above
 sc = SparkContext(conf=conf)
 
-book = sc.textFile("/home/mmanopoli/Udemy/TamingBigDataWithSparkAndPython/data/Book.txt")
-words = book.flatMap(lambda x: x.split())
-wordCounts: dict = words.countByValue()
+# Import and parse the input data into an RDD of lines called lines
+bookLines = sc.textFile("/home/mmanopoli/Udemy/TamingBigDataWithSparkAndPython/data/Book.txt")
+# flatMap is new for us as
+# map transforms each element of an RDD into one new element
+# flatMap can transform each element of an RDD into many new elements
+# In this case, each split out result (text split by spaces) of each line is output as a new element in a new RDD
+bookWords = bookLines.flatMap(lambda x: x.split())
+# We know countByValue - count each element and return a dictionary where the key is the element counted and the value
+#  is the count of that element
+book_word_counts: dict = bookWords.countByValue()
 
-for word, count in wordCounts.items():
+for word, count in book_word_counts.items():
     cleanWord = word.encode(encoding='ascii', errors='ignore')
     if cleanWord:
         print(cleanWord.decode() + " " + str(count))
