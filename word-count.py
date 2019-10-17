e6aafa7c4a5abb40c76036b6952a89e5f3fd841c
mmanopoli <matthew.manopoli@gmail.com>
2019-10-17 15:02:34 +0000

    Adding lecture 16 notes.

--- word-count.py
+++ word-count.py
@@ -1,7 +1,11 @@
+# Objective is to count the number of words in a book
 from pyspark import SparkConf, SparkContext
 
+# Set Master as "local" - local machine + single thread / process
+# Set AppName as "WordCount"
 conf = SparkConf().setMaster("local").setAppName("WordCount")
-sc = SparkContext(conf = conf)
+# Instantiate SparkContext object with conf above
+sc = SparkContext(conf=conf)
 
 input = sc.textFile("file:///sparkcourse/book.txt")
 words = input.flatMap(lambda x: x.split())
