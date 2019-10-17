80e8f56d1cd58f8818b9030a4c3b8a6ff26feb64
mmanopoli <matthew.manopoli@gmail.com>
2019-10-17 15:39:58 +0000

    Adding lecture 17 note files and notes and cleaning up the Python

--- word-count-better.py
+++ word-count-better.py
@@ -1,18 +1,19 @@
 import re
 from pyspark import SparkConf, SparkContext
 
+conf = SparkConf().setMaster("local").setAppName("WordCount")
+sc = SparkContext(conf=conf)
+
+input = sc.textFile("file:///sparkcourse/book.txt")
+
 def normalizeWords(text):
     return re.compile(r'\W+', re.UNICODE).split(text.lower())
 
 
-conf = SparkConf().setMaster("local").setAppName("WordCount")
-sc = SparkContext(conf = conf)
-
-input = sc.textFile("file:///sparkcourse/book.txt")
 words = input.flatMap(normalizeWords)
-wordCounts = words.countByValue()
+wordCounts: dict = words.countByValue()
 
 for word, count in wordCounts.items():
     cleanWord = word.encode('ascii', 'ignore')
-    if (cleanWord):
+    if cleanWord:
         print(cleanWord.decode() + " " + str(count))
