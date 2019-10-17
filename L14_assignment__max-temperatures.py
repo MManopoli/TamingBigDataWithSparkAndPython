2ec4eaa09170d2c48a9b918c150bf6dae15f6285
mmanopoli <matthew.manopoli@gmail.com>
2019-10-17 14:41:23 +0000

    Quickly did Lecture 14 assignment - find the max temperature on any given day

--- L14_assignment__max-temperatures.py
+++ L14_assignment__max-temperatures.py
@@ -1,9 +1,9 @@
-# Objective is to find the minimum temperature observed by various weather stations in the year 1800
+# Objective is to find the maximum temperature observed by various weather stations in the year 1800
 from pyspark import SparkConf, SparkContext
 
 # Set Master as "local" - local machine + single thread / process
-# Set AppName as "MinTemperatures"
-conf = SparkConf().setMaster("local").setAppName("MinTemperatures")
+# Set AppName as "MaxTemperatures"
+conf = SparkConf().setMaster("local").setAppName("MaxTemperatures")
 # Instantiate SparkContext object with conf above
 sc = SparkContext(conf=conf)
 
@@ -22,17 +22,17 @@ lines = sc.textFile("/home/mmanopoli/Udemy/TamingBigDataWithSparkAndPython/data/
 # Use the parseLine function defined above to parse the data into a usable RDD
 parsedLines = lines.map(parseLine)
 # The filter transformation takes a function that returns a boolean
-# In this case, we want the transform to return an RDD where only entries with "TMIN" as the [1] item are kept
-# This is because we are trying to find the minimum observed temperature by each weather station in 1800, and so
-#  we only need to consider the minimum temperature observed on any given day (labeled "TMIN")
-minTemps = parsedLines.filter(lambda x: "TMIN" in x[1])
-# Strip out the entryType because it's always "TMIN" now
+# In this case, we want the transform to return an RDD where only entries with "TMAX" as the [1] item are kept
+# This is because we are trying to find the maximum observed temperature by each weather station in 1800, and so
+#  we only need to consider the maximum temperature observed on any given day (labeled "TMAX")
+minTemps = parsedLines.filter(lambda x: "TMAX" in x[1])
+# Strip out the entryType because it's always "TMAX" now
 # This is now a key value pair, stationID = key, temperature = value
 stationTemps = minTemps.map(lambda x: (x[0], x[2]))
 # reduce by key = for each key reduce down using the lambda function
 # So, the lambda function returns the minimum of two inputs, x and y, which defines the result when two elements
 #  x and y are reduced/combined for a given key
-minTemps = stationTemps.reduceByKey(lambda x, y: min(x, y))
+minTemps = stationTemps.reduceByKey(lambda x, y: max(x, y))
 # Collect action - collect the results into a Python list
 results = minTemps.collect()
 
