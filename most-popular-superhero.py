f22f6907d2875f7e091ac201dfce957a02b279cd
mmanopoli <matthew.manopoli@gmail.com>
2019-10-18 15:45:30 +0000

    Adding lecture 21 note files and notes and cleaning up the Python

--- most-popular-superhero.py
+++ most-popular-superhero.py
@@ -1,13 +1,14 @@
 from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setMaster("local").setAppName("PopularHero")
+conf = SparkConf().setMaster("local[4]").setAppName("PopularHero")
 sc = SparkContext(conf=conf)
 
-def countCoOccurences(line):
-    elements = line.split()
-    return (int(elements[0]), len(elements) - 1)
-
-
+# Marvel-Names.txt data format:
+# first value: Marvel hero ID
+# Second value: Marvel hero name
+#
+# Example:
+# 2549 "HULK III/BRUCE BANNE"
 def parseNames(line):
     fields = line.split('\"')
     return (int(fields[0]), fields[1].encode("utf8"))
@@ -15,6 +16,17 @@ def parseNames(line):
 
 names = sc.textFile("/home/mmanopoli/Udemy/TamingBigDataWithSparkAndPython/data/Marvel-Names.txt")
 namesRdd = names.map(parseNames)
+
+# Marvel-Graph.txt data format:
+# First value = Marvel hero ID
+# Remaining values = hero IDs the first hero ID has appeared with
+#
+# Example:
+# 5983 1165 3836 4361 1282 716 4289 4646 6300 5084 2397 4454 1913 5861 5485
+def countCoOccurences(line):
+    elements = line.split()
+    return (int(elements[0]), len(elements) - 1)
+
 
 lines = sc.textFile("/home/mmanopoli/Udemy/TamingBigDataWithSparkAndPython/data/Marvel-Graph.txt")
 pairings = lines.map(countCoOccurences)
