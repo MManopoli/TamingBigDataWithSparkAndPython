40773d633c8b2251a27eefd4be9d1174f7aaee93
mmanopoli <matthew.manopoli@gmail.com>
2019-10-18 15:46:51 +0000

    Adding lecture 21 note files and notes and cleaning up the Python

--- most-popular-superhero.py
+++ most-popular-superhero.py
@@ -11,7 +11,7 @@ sc = SparkContext(conf=conf)
 # 2549 "HULK III/BRUCE BANNE"
 def parseNames(line):
     fields = line.split('\"')
-    return (int(fields[0]), fields[1].encode("utf8"))
+    return int(fields[0]), fields[1].encode("utf8")
 
 
 names = sc.textFile("/home/mmanopoli/Udemy/TamingBigDataWithSparkAndPython/data/Marvel-Names.txt")
@@ -19,13 +19,15 @@ namesRdd = names.map(parseNames)
 
 # Marvel-Graph.txt data format:
 # First value = Marvel hero ID
-# Remaining values = hero IDs the first hero ID has appeared with
+# Remaining values = hero IDs the first hero ID has appeared with in comic books
+#
+# Note: Hero IDs can span multiple lines
 #
 # Example:
 # 5983 1165 3836 4361 1282 716 4289 4646 6300 5084 2397 4454 1913 5861 5485
 def countCoOccurences(line):
     elements = line.split()
-    return (int(elements[0]), len(elements) - 1)
+    return int(elements[0]), (len(elements) - 1)
 
 
 lines = sc.textFile("/home/mmanopoli/Udemy/TamingBigDataWithSparkAndPython/data/Marvel-Graph.txt")
