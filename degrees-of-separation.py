7800ba3b370323c733bbbd81134e70d391b20d82
mmanopoli <matthew.manopoli@gmail.com>
2019-10-19 02:30:26 +0000

    Adding lecture 25 note files and notes and cleaning up the Python

--- degrees-of-separation.py
+++ degrees-of-separation.py
@@ -83,6 +83,8 @@ def bfsMap(node):
                 hitCounter.add(1)
 
             # Create the updated node
+            # Note here that the updated node is distinct from the original in that it has no connections listed,
+            #  this will be important later when reducing the nodes by key == heroID
             newEntry = (newCharacterID, ([], newDistance, newColor))
 
             # Append the updated node to the list of node updates (== results)
@@ -100,20 +102,25 @@ def bfsMap(node):
     return results
 
 
+# Reduce / consolidate distances by key == heroID to preserve the darkest color and shortest path for each node
 def bfsReduce(data1, data2):
-    edges1 = data1[0]
-    edges2 = data2[0]
-    distance1 = data1[1]
-    distance2 = data2[1]
-    color1 = data1[2]
-    color2 = data2[2]
+    # data1 and data2 are both node VALUES of the form: (connections, distance, color)
 
+    edges1 = data1[0]  # data1 connections
+    edges2 = data2[0]  # data2 connections
+    distance1 = data1[1]  # data1 distance
+    distance2 = data2[1]  # data2 distance
+    color1 = data1[2]  # data1 color
+    color2 = data2[2]  # data2 color
+
+    # Set the default distance, color, and edges
     distance = 9999
-    color = color1
-    edges = []
+    color = color1  # data1 color
+    edges = []  # connections
 
     # See if one is the original node with its connections.
-    # If so preserve them.
+    #   If so, then preserve them.
+    # Remember from above that any node created as a "result" (== an update) will have connections == []
     if (len(edges1) > 0):
         edges.extend(edges1)
     if (len(edges2) > 0):
@@ -122,44 +129,47 @@ def bfsReduce(data1, data2):
     # Preserve minimum distance
     if (distance1 < distance):
         distance = distance1
-
     if (distance2 < distance):
         distance = distance2
 
     # Preserve darkest color
+    # First, check color1 against color2 to acquire the darkest color from color2
     if (color1 == 'WHITE' and (color2 == 'GRAY' or color2 == 'BLACK')):
         color = color2
-
     if (color1 == 'GRAY' and color2 == 'BLACK'):
         color = color2
 
+    # Second, check color2 against color1 and get the darkest color from color2
     if (color2 == 'WHITE' and (color1 == 'GRAY' or color1 == 'BLACK')):
         color = color1
-
     if (color2 == 'GRAY' and color1 == 'BLACK'):
         color = color1
 
+    # Return the appropriately combined data - that is, the combination of all updates with the original node
     return (edges, distance, color)
 
 
-#Main program here:
+# Main program starts here:
+
+# Create the starting RDD with all of the nodes
 iterationRdd = createStartingRdd()
 
+# We assume we will never have more distance than 9 - so iterate over range(0, 10)
 for iteration in range(0, 10):
-    print("Running BFS iteration# " + str(iteration+1))
+    print("Running BFS iteration # " + str(iteration+1))
 
     # Create new vertices as needed to darken or reduce distances in the
-    # reduce stage. If we encounter the node we're looking for as a GRAY
-    # node, increment our accumulator to signal that we're done.
+    #  reduce stage. If we encounter the node we're looking for as a GRAY
+    #  node, then the accumulator will the incremented to signal that we're done.
     mapped = iterationRdd.flatMap(bfsMap)
 
     # Note that mapped.count() action here forces the RDD to be evaluated, and
-    # that's the only reason our accumulator is actually updated.
+    #  that's the only reason our accumulator is actually updated.
     print("Processing " + str(mapped.count()) + " values.")
 
-    if (hitCounter.value > 0):
-        print("Hit the target character! From " + str(hitCounter.value) \
-            + " different direction(s).")
+    # Since the count is an action and we have updated the accumulator -> check if we found the target!
+    if hitCounter.value > 0:
+        print("Hit the target character in this iteration from " + str(hitCounter.value) + " different direction(s)!")
         break
 
     # Reducer combines data for each character ID, preserving the darkest
