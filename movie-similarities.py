c2d8f162db11729d408a72122c289deca857aec4
mmanopoli <matthew.manopoli@gmail.com>
2019-11-01 16:54:06 +0000

    Adding lecture 27 code/note files and adding notes and updating the notes and cleaning up the Python.  This lecture covers joins, cache, persist, and a number of other important operations.

--- movie-similarities.py
+++ movie-similarities.py
@@ -56,7 +56,7 @@ sc = SparkContext(conf = conf)
 print("\nLoading movie names...")
 nameDict = loadMovieNames()
 
-data = sc.textFile("file:///SparkCourse/ml-100k/u.data")
+data = sc.textFile("/home/mmanopoli/Udemy/TamingBigDataWithSparkAndPython/data/ml-100k/u.data")
 
 # Map ratings to key / value pairs: user ID => movie ID, rating
 ratings = data.map(lambda l: l.split()).map(lambda l: (int(l[0]), (int(l[1]), float(l[2]))))
