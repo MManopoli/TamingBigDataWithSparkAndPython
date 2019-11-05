936cf002e2ed8b9cc0ca1e42b1cb8620d48f216c
mmanopoli <matthew.manopoli@gmail.com>
2019-11-05 18:00:54 +0000

    Adding lecture 40-41 code/note files and adding notes and updating the notes and cleaning up the Python.  This lecture covers using the MLLib and it's Alternating Least Squares (ALS) algorithm to find movie recommendations.

--- movie-recommendations-als.py
+++ movie-recommendations-als.py
@@ -2,30 +2,47 @@ import sys
 from pyspark import SparkConf, SparkContext
 from pyspark.mllib.recommendation import ALS, Rating
 
+# u.item columns:
+#
+# Movie ID|Movie Name|....
+# 1|Toy Story (1995)|01-Jan-1995||http://us.imdb.com/M/title-exact?Toy%20Story%20(1995)|0|0|0|1|1|1|0|0|...
+# 2|GoldenEye (1995)|01-Jan-1995||http://us.imdb.com/M/title-exact?GoldenEye%20(1995)|0|1|1|0|0|0|0|0|0|...
 def loadMovieNames():
     movieNames = {}
-    with open("ml-100k/u.ITEM", encoding='ascii', errors="ignore") as f:
+    with open(
+            "/home/mmanopoli/Udemy/TamingBigDataWithSparkAndPython/data/ml-100k/u.item",
+            encoding='iso-8859-1'
+    ) as f:
         for line in f:
             fields = line.split('|')
             movieNames[int(fields[0])] = fields[1]
     return movieNames
 
+
 conf = SparkConf().setMaster("local[*]").setAppName("MovieRecommendationsALS")
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
+
 sc.setCheckpointDir('checkpoint')
 
 print("\nLoading movie names...")
 nameDict = loadMovieNames()
 
-data = sc.textFile("file:///SparkCourse/ml-100k/u.data")
+data = sc.textFile("/home/mmanopoli/Udemy/TamingBigDataWithSparkAndPython/data/ml-100k/u.data")
 
+# u.data columns:
+#
+# User ID, Movie ID, Rating, Timestamp
+# 196	242	3	881250949
+# 186	302	3	891717742
+# 22	377	1	878887116
+#
+# Map to a MLLib Rating data structure
 ratings = data.map(lambda l: l.split()).map(lambda l: Rating(int(l[0]), int(l[1]), float(l[2]))).cache()
 
 # Build the recommendation model using Alternating Least Squares
 print("\nTraining recommendation model...")
 rank = 10
-# Lowered numIterations to ensure it works on lower-end systems
-numIterations = 6
+numIterations = 20
 model = ALS.train(ratings, rank, numIterations)
 
 userID = int(sys.argv[1])
